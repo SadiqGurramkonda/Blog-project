ebd58397f5e56ef9be41221412c1f8ee49b19b43
Sadiq Gurramkonda <gurramkondasadiq216@gmail.com>
2024-03-29 23:59:20 +0530

    added skeleton component

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,3 +1,4 @@
+import { Button } from "./Auth"
 import { Avatar } from "./BlogCard"
 import { Link } from "react-router-dom"
 
@@ -5,14 +6,22 @@ export const Header = () => {
 
     return (
         <div>
-            <div className="flex  justify-between px-2 py-2 border-b">
-                <Link to={"/blogs"}>
+            <div className="flex justify-between px-2 py-2 border-b">
+                
                     <div className="flex flex-col justify-center font-black text-xl">
+                    <Link to={"/blogs"}>
                         MEDIUM
+                    </Link>
                     </div>
-                </Link>
-                <div>
-                    <Avatar name="SDQ" size="big"></Avatar>
+                <div className="flex">
+                    <div className="px-10">
+                        <Link to={"/publish"}>
+                         <Button onClick={()=>{}} label="Create" />
+                        </Link>
+                        </div>
+                    <div className=" flex flex-col justify-center">
+                        <Avatar name="SDQ" size="big"></Avatar>
+                    </div>
                 </div>
             </div>
         </div>
---
+++ frontend/src/components/Loader.tsx
@@ -0,0 +1,57 @@
+export const Loader = () => {
+
+    return (
+            <div className="p-4 space-y-4 border border-gray-100 divide-y divide-gray-100 rounded shadow animate-pulse max-h-screen md:p-6">
+                <div className="flex items-center justify-between">
+                    <div>
+                        <div className="h-10 bg-gray-100 rounded-full  w-full mb-2.5"></div>
+                        <div className="w-full h-2 bg-gray-100 rounded-full "></div>
+                    </div>
+                    <div className="h-10 bg-gray-100 rounded-full  w-full"></div>
+                </div>
+                <div className="flex items-center justify-between pt-4">
+                    <div>
+                        <div className="h-10 bg-gray-100 rounded-full  w-full mb-2.5"></div>
+                        <div className="w-full h-2 bg-gray-100 rounded-full "></div>
+                    </div>
+                    <div className="h-10 bg-gray-100 rounded-full  w-full"></div>
+                </div>
+                <div className="flex items-center justify-between pt-4">
+                    <div>
+                        <div className="h-10 bg-gray-100 rounded-full  w-full mb-2.5"></div>
+                        <div className="w-full h-2 bg-gray-100 rounded-full "></div>
+                    </div>
+                    <div className="h-10 bg-gray-100 rounded-full  w-full"></div>
+                </div>
+                <div className="flex items-center justify-between pt-4">
+                    <div>
+                        <div className="h-10 bg-gray-100 rounded-full  w-full mb-2.5"></div>
+                        <div className="w-full h-2 bg-gray-100 rounded-full"></div>
+                    </div>
+                    <div className="h-10 bg-gray-100 rounded-full w-full"></div>
+                </div>
+                <div className="flex items-center justify-between pt-4">
+                    <div>
+                        <div className="h-10 bg-gray-100 rounded-full w-full mb-2.5"></div>
+                        <div className="w-full h-2 bg-gray-100 rounded-full"></div>
+                    </div>
+                    <div className="h-10 bg-gray-100 rounded-full  w-full"></div>
+                </div>
+                <div className="flex items-center justify-between pt-4">
+                    <div>
+                        <div className="h-10 bg-gray-100 rounded-full w-full mb-2.5"></div>
+                        <div className="w-full h-2 bg-gray-100 rounded-full"></div>
+                    </div>
+                    <div className="h-10 bg-gray-100 rounded-full  w-full"></div>
+                </div>
+                <div className="flex items-center justify-between pt-4">
+                    <div>
+                        <div className="h-10 bg-gray-100 rounded-full w-full mb-2.5"></div>
+                        <div className="w-full h-2 bg-gray-100 rounded-full"></div>
+                    </div>
+                    <div className="h-10 bg-gray-100 rounded-full  w-full"></div>
+                </div>
+                <span className="sr-only">Loading...</span>
+            </div>
+    )
+}
